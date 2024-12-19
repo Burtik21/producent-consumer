f97f82a2c7e7316d39235b9fb30c6773e325c279
Šimon Stopa <127181329+Burtik21@users.noreply.github.com>
2024-12-19 20:47:31 +0100

    Delete producent-consumer directory

--- producent-consumer/reset_deck_request.py
+++
@@ -1,9 +0,0 @@
-from .request import Request  # Importujeme třídu Request
-
-# Třída pro reset balíčku
-class ResetDeck(Request):
-    def __init__(self):
-        super().__init__(priority=3)
-
-    def process(self):
-        print(f"Shuffling cards")
